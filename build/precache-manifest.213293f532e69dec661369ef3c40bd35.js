1ff735071324664ac93aff6ce5bd499b19c982bd
Anthony D <dunahee@gmail.com>
2019-02-06 14:10:21 -0600

    production build

---
+++ build/precache-manifest.213293f532e69dec661369ef3c40bd35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d360b915034601e77729",
+    "url": "/static/css/main.c21e26ae.chunk.css"
+  },
+  {
+    "revision": "d360b915034601e77729",
+    "url": "/static/js/main.d360b915.chunk.js"
+  },
+  {
+    "revision": "6a24ebe9e34e7a8b8a0c",
+    "url": "/static/js/1.6a24ebe9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  },
+  {
+    "revision": "2509b9e819ca04373f2fa47bd0542296",
+    "url": "/index.html"
+  }
+];
